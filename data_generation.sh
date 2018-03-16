3ebfd14f05e1c9f2edef9500fe9136f450763f3c
pabrodbra <pabrod@uma.es>
2018-03-16 20:17:01 +0100

    data generation semifull

--- data_generation.sh
+++ data_generation.sh
@@ -5,32 +5,43 @@
 MULTIFASTA=$1
 N_READS=$2
 
+if [ "$#" -ne 2 ]; then
+	echo "***USAGE*** data_generation.sh <multifasta> <num_reads>"
+	exit 1
+fi
 
-# RACKIT binary
-SH_DIR=$(dirname "$0")
-BIN="${SH_DIR}/bin/"
-# MEGAHIT Path
+### ---------
+# Tools
 MEGAHIT=${MEGAHIT}
 GRINDER=grinder
 # Output Directory Path
 OUTPUT="/home/pablorod/results/rac_test/"
+INTERMEDIATE_FILES="${OUTPUT}intermediateFiles/"
+ASM="${INTERMEDIATE_FILES}data_generation/"
 # Output folders
-RESULTS="${OUTPUT}inputs/"
-READS_FQ="${RESULTS}reads.fq"
-READS_FA="${RESULTS}reads.fa"
-CONTIGS="${RESULTS}contigs.fa"
+INPUTS="${OUTPUT}inputs/"
+# Output names
+READS_FQ="${INPUTS}reads.fastq"
+READS_FA="${INPUTS}reads.fasta"
+CONTIGS="${INPUTS}contigs.fa"
 
-#mkdir $RESULTS
-#grinder -rf test_bacterial.fa -tr 10000 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -ql 30 10 -fq 1
-# Fasta
-#${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform
+### Folder checks
+# Intermediate files
+mkdir -p $INTERMEDIATE_FILES
+# Inputs
+if [ -d $INPUTS ]; then rm -Rf $INPUTS; fi
+mkdir -p $INPUTS
+# Assembly
+if [ -d $ASM ]; then rm -Rf $ASM; fi
+
+### Execution
+# Fasta ${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform
 # FastQ
-${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -od ${RESULTS} -ql 30 10 -fq 1
-mv "${RESULTS}grinder-reads.fastq" ${READS_FQ}
-rm "${RESULTS}/grinder*"
+${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -od ${ASM} -ql 30 10 -fq 1
+mv "${ASM}grinder-reads.fastq" ${READS_FQ}
 # Fastq to Fasta
 sed -n '1~4s/^@/>/p;2~4p' ${READS_FQ} > ${READS_FA}
 
 # MEGAHIT - Assembly
-rm -r $CONTIGS
-${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o ${CONTIGS} #-t 12
+${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o ${ASM} #-t 12
+mv "${ASM}final.contigs.fa" $CONTIGS
