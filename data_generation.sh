fb68af0923198ea74912b02d3c8b6d8957c4f34f
pabrodbra <pabrod@uma.es>
2018-03-16 17:19:59 +0100

    data generation mods

--- data_generation.sh
+++ data_generation.sh
@@ -16,20 +16,21 @@ GRINDER=grinder
 OUTPUT="/home/pablorod/results/rac_test/"
 # Output folders
 RESULTS="${OUTPUT}inputs/"
-READS_FQ="${RESULTS}reads.fa"
+READS_FQ="${RESULTS}reads.fq"
 READS_FA="${RESULTS}reads.fa"
 CONTIGS="${RESULTS}contigs.fa"
 
-mkdir $RESULTS
+#mkdir $RESULTS
 #grinder -rf test_bacterial.fa -tr 10000 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -ql 30 10 -fq 1
 # Fasta
-${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform
+#${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform
 # FastQ
-${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -ql 30 10 -fq 1
-mv "${SH_DIR}/grinder-reads.fastq" ${READS_FQ}
-rm "${SH_DIR}/grinder*"
+${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -od ${RESULTS} -ql 30 10 -fq 1
+mv "${RESULTS}grinder-reads.fastq" ${READS_FQ}
+rm "${RESULTS}/grinder*"
 # Fastq to Fasta
 sed -n '1~4s/^@/>/p;2~4p' ${READS_FQ} > ${READS_FA}
 
 # MEGAHIT - Assembly
-${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o CONTIGS #-t 12
+rm -r $CONTIGS
+${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o ${CONTIGS} #-t 12
