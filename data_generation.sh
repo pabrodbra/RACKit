16f70dfa158a71d3030873d34f2097f89654bebe
pabrodbra <pabrod@uma.es>
2018-03-17 05:05:01 +0100

    Added steps to results generation

--- data_generation.sh
+++ data_generation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Synth Data Generation Script by Pablo Rodriguez
 
-# datageneration.sh <multifasta> <num_reads>
 MULTIFASTA=$1
 N_READS=$2
+OUTPUT_DIRECTORY=$3
 
-if [ "$#" -ne 2 ]; then
-	echo "***USAGE*** data_generation.sh <multifasta> <num_reads>"
+if [ "$#" -ne 3 ]; then
+	echo "***USAGE*** data_generation.sh <multifasta> <num_reads> <output_dir>"
 	exit 1
 fi
 
@@ -15,9 +15,10 @@ fi
 MEGAHIT=${MEGAHIT}
 GRINDER=grinder
 # Output Directory Path
-OUTPUT="/home/pablorod/results/rac_test/"
+OUTPUT=${OUTPUT_DIRECTORY}
 INTERMEDIATE_FILES="${OUTPUT}intermediateFiles/"
 ASM="${INTERMEDIATE_FILES}data_generation/"
+ASSEMBLY="${ASM}asm/"
 # Output folders
 INPUTS="${OUTPUT}inputs/"
 # Output names
@@ -33,15 +34,22 @@ if [ -d $INPUTS ]; then rm -Rf $INPUTS; fi
 mkdir -p $INPUTS
 # Assembly
 if [ -d $ASM ]; then rm -Rf $ASM; fi
+mkdir -p $ASM
+if [ -d $ASSEMBLY ]; then rm -Rf $ASSEMBLY; fi
 
 ### Execution
+: '
 # Fasta ${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform
 # FastQ
 ${GRINDER} -rf $1 -tr $2 -rd 101 uniform 10 -md poly4 3e-3 3.3e-8 -mr 80 20 -am uniform -od ${ASM} -ql 30 10 -fq 1
 mv "${ASM}grinder-reads.fastq" ${READS_FQ}
 # Fastq to Fasta
 sed -n '1~4s/^@/>/p;2~4p' ${READS_FQ} > ${READS_FA}
+'
+echo "(1/2) FASTQ and FASTA reads generated"
 
 # MEGAHIT - Assembly
-${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o ${ASM} #-t 12
-mv "${ASM}final.contigs.fa" $CONTIGS
+${MEGAHIT} --kmin-1pass --k-min 27 --k-step 10 --k-max 87 -r ${READS_FA} -o ${ASSEMBLY} #-t 12
+mv "${ASSEMBLY}final.contigs.fa" $CONTIGS
+echo "(2/2) Contigs assembled..."
+
--- rackit.sh
+++ rackit.sh
@@ -7,8 +7,6 @@
 # RACKIT binary
 SH_DIR=$(dirname "$0")
 BIN="${SH_DIR}/bin/"
-# MEGAHIT Path
-MEGAHIT=0
 # MAKEBLASTDB Path
 MAKEBLASTDB=makeblastdb
 # BLASTN Path
@@ -17,6 +15,8 @@ BLASTN=blastn
 MEGAN=0
 # QNUCLPARSERBLAST Path
 QNUCLPARSERBLAST="${BIN}qnuclparserblast"
+# FILTERPARSEDBLAST Path
+FILTERPARSEDBLAST="${BIN}filterParsedBlast"
 # TAXOMAKER Path
 TAXOMAKER="${BIN}taxomaker"
 # MERGEFULLFASTA Path
@@ -30,10 +30,13 @@ RPATH=0
 ### Inputs
 #############
 # Reads fasta Path
-READS="/home/pablorod/data/metagenome/RAC_test/ssd_reads_v2.fa"
-CONTIGS="/home/pablorod/data/metagenome/RAC_test/final.contigs.fa"
+#READS="/home/pablorod/data/metagenome/RAC_test/ssd_reads_v2.fa"
+#CONTIGS="/home/pablorod/data/metagenome/RAC_test/final.contigs.fa"
+READS=$1
+CONTIGS=$2
 # Database fasta Path
-DB="/home/pablorod/data/metagenome/RAC_test/refsoilDB.fa"
+#DB="/home/pablorod/data/metagenome/RAC_test/refsoilDB.fa"
+DB=$3
 # Blast Params
 BLASTN_EVALUE=0.0001
 
@@ -41,8 +44,8 @@ BLASTN_EVALUE=0.0001
 ### Outputs
 #############
 # Output Directory Path
-OUTPUT="/home/pablorod/results/rac_test/"
-
+#OUTPUT="/home/pablorod/results/rac_test/"
+OUTPUT=$4
 # Output folders
 INTERMEDIATE_FILES="${OUTPUT}intermediateFiles/"
 RESULTS="${OUTPUT}results/"
@@ -56,28 +59,23 @@ CONTIGDB_NAME="CONTIGDB"
 REFDB_PATH="${INTERMEDIATE_FILES}${REFDB_NAME}"
 CONTIGDB_PATH="${INTERMEDIATE_FILES}${CONTIGDB_NAME}"
 
+if [ "$#" -ne 4 ]; then
+	echo "****USAGE**** rackit.sh <reads> <contigs> <reference_db> <output>
 
 ##########################
 ### --- Execution --- ####
 ##########################
+mkdir -p $INTERMEDIATE_FILES
+mkdir -p $RESULTS
 
-# DEBUG
-: '
-rm -r $INTERMEDIATE_FILES
-rm -r $RESULTS
-
-'
 ### Make blast databases
 # STATUS: WORKING (WORKING/TESTING)
-# OPTIONAL: NO (YES/NO)
+# OPTIONAL: YES (YES/NO)
 : '
-mkdir $INTERMEDIATE_FILES
-mkdir $RESULTS
-
 ${MAKEBLASTDB} -in ${DB} -dbtype nucl -title ${REFDB_NAME} \
     -max_file_sz 2GB -out ${REFDB_PATH} &>> ${LOG}
 
-# OPTIONAL: YES (YES/NO)
+# OPTIONAL: NO (YES/NO)
 ${MAKEBLASTDB} -in ${CONTIGS} -dbtype nucl -title ${CONTIGDB_NAME} \
     -max_file_sz 2GB -out ${CONTIGDB_PATH} &>> ${LOG}
 
@@ -99,16 +97,29 @@ ${BLASTN} -task megablast -db ${REFDB_PATH} -evalue ${BLASTN_EVALUE} -ungapped \
 ${BLASTN} -task megablast -db ${CONTIGDB_PATH} -evalue ${BLASTN_EVALUE} -ungapped \
     -query ${READS} -out ${READS_CONTIGS_BLAST_PATH} &>> ${LOG}
 '
-### Parse blast results
+### Parse blast results + Filter
 # STATUS: TESTING (WORKING/TESTING)
 # OPTIONAL: NO (YES/NO)
 PARSED_READS_REFDB_BLAST_PATH="${INTERMEDIATE_FILES}READS_VS_REFDB.pblast"
 PARSED_CONTIGS_REFDB_BLAST_PATH="${INTERMEDIATE_FILES}CONTIGS_VS_REFDB.pblast"
 PARSED_READS_CONTIGS_BLAST_PATH="${INTERMEDIATE_FILES}READS_VS_CONTIGS.pblast"
+FILTER_PARSED_READS_CONTIGS="${INTERMEDIATE_FILES}FILTER_READS_VS_CONTIGS.pblast"
 
+# QNUCLPARSERBLAST
 ${QNUCLPARSERBLAST} ${READS_REFDB_BLAST_PATH} ${PARSED_READS_REFDB_BLAST_PATH} &>> ${LOG}
 ${QNUCLPARSERBLAST} ${CONTIGS_REFDB_BLAST_PATH} ${PARSED_CONTIGS_REFDB_BLAST_PATH} &>> ${LOG}
 ${QNUCLPARSERBLAST} ${READS_CONTIGS_BLAST_PATH} ${PARSED_READS_CONTIGS_BLAST_PATH} &>> ${LOG}
+
+# FIX PARSEDBLAST
+FIX_READS="{INTERMEDEIATE_FILES}fixed_reads.pblast"
+FIX_CONTIGS="{INTERMEDEIATE_FILES}fixed_contigs.pblast"
+FIX_READS_CONTIGS="${INTERMEDIATE_FILES}fixed_reads_contigs.pblast"
+sed -z 's/\n>/;>/g' ${PARSED_READS_REFDB_BLAST_PATH} | sed -z 's/\n/ /g' | sed -z 's/;/\n/g' | sed -z 's/\t/;/g' > ${FIX_READS}
+sed -z 's/\n>/;>/g' ${PARSED_CONTIGS_REFDB_BLAST_PATH} | sed -z 's/\n/ /g' | sed -z 's/;/\n/g' | sed -z 's/\t/;/g' > ${FIX_CONTIGS}
+sed -z 's/\n>/;>/g' ${PARSED_READS_CONTIGS_BLAST_PATH} | sed -z 's/\n/ /g' | sed -z 's/;/\n/g' | sed -z 's/\t/;/g' > ${FIX_READS_CONTIGS}
+
+FILTER_READS="${INTERMEDIATE_FILES}filtered_reads_contigs.pblast"
+${FILTERPARSEDBLAST} ${FIX_READS_CONTIGS} ${FILTER_READS} 0 # or 1
 
 ### MEGAN
 # STATUS: TESTING (WORKING/TESTING)
@@ -119,8 +130,21 @@ ${QNUCLPARSERBLAST} ${READS_CONTIGS_BLAST_PATH} ${PARSED_READS_CONTIGS_BLAST_PAT
 # OPTIONAL: NO (YES/NO)
 
 ### DB Coverage
+# TAXOMAKER
 # STATUS: TESTING (WORKING/TESTING)
-# OPTIONAL: NO (YES/NO)
+# OPTIONAL: YES (YES/NO)
+UNI_DB="${INTERMEDIATE_FILES}UNI_REFDB.fa"
+TAXO_FILE=""
+${TAXOMAKER} ${DB} 0
+
+# MERGEMULTIFAST
+${MERGEFULLFASTA} ${DB} ${UNI_DB}
+
+# UNISEQDBCOVERAGE
+FIX_READS="{INTERMEDEIATE_FILES}fixed_reads.pblast"
+FIX_CONTIGS="{INTERMEDEIATE_FILES}fixed_contigs.pblast"
+COVERAGE_OUTPUT="${RESULTS}coverage.info"
+${UNISEQDBCOVERAGE} ${FIX_READS} ${FIX_CONTIGS} ${TAXO_FILE} ${UNI_DB} ${COVERAGE_OUTPUT}
 
 ### R Results
 # STATUS: TESTING (WORKING/TESTING)
